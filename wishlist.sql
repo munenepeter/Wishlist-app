a8901bb77b27d70987519360567ca6d8cad7876f
Peter Munene <peternjega7@gmail.com>
2021-07-26 22:53:05 +0300

    Update Database

--- wishlist.sql
+++ wishlist.sql
@@ -25,14 +25,42 @@ CREATE TABLE IF NOT EXISTS `products` (
   `product_price` int(10) NOT NULL,
   `product_qty` int(10) NOT NULL,
   KEY `PRIMARY KEY` (`product_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
--- Dumping data for table wishlist.products: ~2 rows (approximately)
+-- Dumping data for table wishlist.products: ~4 rows (approximately)
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
 INSERT INTO `products` (`product_id`, `product_name`, `product_img`, `product_desc`, `product_price`, `product_qty`) VALUES
-	(1, 'Hand Wash', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCORAlZ7dAD_2Ccm4TDpJaiYIUkHqJ1ZPzaw&usqp=CAU', 'This is a hand wash used in  cleaning. It has a very high cleaning effect', 300, 200),
-	(2, 'Dish Wash', 'https://lh3.googleusercontent.com/IqikVgs86HCvBYzFZwrs-4zn4fmkjU3LgWGkBdq08YycM8MrU3hgXXJsqdZdQwbx9pUQXCM=s85', 'This is a detergent used in utensils cleaning. It has a very high rathering effect', 100, 23);
+	(1, 'Hand Wash', 'https://via.placeholder.com/150', 'This is a hand wash used in  cleaning. It has a very high cleaning effect, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae fugiat delectus architecto adipisci praesentium.', 300, 200),
+	(2, 'Dish Wash', 'https://via.placeholder.com/150', 'This is a detergent used in utensils cleaning. It has a very high rathering effect', 100, 23),
+	(3, 'Item 3', 'https://via.placeholder.com/150', 'This is item 3 You can create a variable to store the context of the current element', 50, 10),
+	(4, 'Item 4', 'https://via.placeholder.com/150', 'This is item 4 Specify the width first, then height. Height is optional: if no height is specified, your placeholder image will be a square. So this example', 60, 60);
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
+
+-- Dumping structure for table wishlist.wishlist
+CREATE TABLE IF NOT EXISTS `wishlist` (
+  `wishlist_id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) NOT NULL,
+  `product_size` varchar(50) DEFAULT '',
+  `product_qty` varchar(11) NOT NULL,
+  `product_price` varchar(11) NOT NULL,
+  KEY `PRIMARY KEY` (`wishlist_id`),
+  KEY `FK__products` (`product_id`),
+  CONSTRAINT `FK__products` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+
+-- Dumping data for table wishlist.wishlist: ~0 rows (approximately)
+/*!40000 ALTER TABLE `wishlist` DISABLE KEYS */;
+INSERT INTO `wishlist` (`wishlist_id`, `product_id`, `product_size`, `product_qty`, `product_price`) VALUES
+	(1, 1, '0.5 litre', '3', '900'),
+	(2, 1, '0.5 litre', '4', '1200'),
+	(3, 2, '5 litre', '3', '300'),
+	(4, 2, '0.5 litre', '11', '1100'),
+	(5, 2, '0.5 litre', '11', '1100'),
+	(6, 2, '0.5 litre', '11', '1100'),
+	(7, 2, '0.5 litre', '11', '1100'),
+	(8, 2, '0.5 litre', '11', '1100'),
+	(9, 2, '0.5 litre', '11', '1100');
+/*!40000 ALTER TABLE `wishlist` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
