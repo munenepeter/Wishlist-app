23dd4b01d513ccca684b5dd976017ec46aa962c6
Peter Munene <peternjega7@gmail.com>
2021-07-28 20:30:50 +0300

    Change the look of index

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <title>All Products</title>
     <link rel="stylesheet" href="assets/style.css">
     <link rel="Home" href="index.html">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.7/tailwind.min.css" />
 </head>
 
 <body>
@@ -44,34 +45,83 @@
                 </select>
             </div>
             <?php
-                // var_dump($_SESSION['wishlist']);
-                //  var_dump(Request::getwishlist());
+            // var_dump($_SESSION['wishlist']);
+            //  var_dump(Request::getwishlist());
 
             //   Her i get the products in the Database
 
             $products = App::get('database')->selectAll('products');
 
+
+
+
+
             ?>
-            <div class="row">
-                <?php foreach ($products as $product) : ?>
-                    
-                    <div class="col-4">
-                        <img src="<?= $product['product_img'] ?>">
-                        <div class="flex">
-                            <div class="text">
-                                <h4><?= $product['product_name'] ?></h4>
-                                <p>ksh.<?= $product['product_price'] ?>.00</p>
 
-                                <a href="productdetail.php?product=<?= $product['product_id'] ?>">View Product</a>
+
+
+
+
+
+
+
+
+
+
+
+            <section class="text-gray-600 body-font">
+                <div class="container px-5 py-24 mx-auto">
+                    <div class="flex flex-wrap -m-4">
+                        <?php foreach ($products as $product) : ?>
+                            <div class="p-4 md:w-1/3">
+                                <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                                    <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="<?= $product['product_img'] ?>" alt="blog">
+                                    <div class="p-6">
+                                        <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
+                                        <h1 class="title-font text-lg font-medium text-gray-900 mb-3"><?= $product['product_name'] ?></h1>
+
+                                        <p>ksh.<?= $product['product_price'] ?>.00</p>
+                                        <div class="flex items-center flex-wrap ">
+                                            <a href="productdetail.php?product=<?= $product['product_id'] ?>" class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">View Product
+
+                                            </a>
+                                            <span class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                                                <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                                                    <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                                                    <circle cx="12" cy="12" r="3"></circle>
+                                                </svg>
+                                            </span>
+                                            <span class="text-gray-400 inline-flex items-center leading-none text-sm">
+                                                <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                                                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                                                </svg>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                            
-                        </div>
-
-
+                        <?php endforeach ?>
                     </div>
-                <?php endforeach ?>
+                </div>
+            </section>
 
-            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
             <div class="page-btn">
                 <span>1</span>
                 <span>2</span>
