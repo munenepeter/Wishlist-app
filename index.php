7d2048f4d62b83e034f1d7cead095bedc7d17c4b
Peter Munene <peternjega7@gmail.com>
2021-07-28 21:08:12 +0300

    Facelift of index

--- index.php
+++ index.php
@@ -13,124 +13,105 @@
 </head>
 
 <body>
-
-    <div class="body">
-        <div class="container">
-            <div class="navbar">
-                <div class="logo">
-                    <a href="index.html"><img src="assets/images/logo.png" width="125px"></a>
-                </div>
-                <nav>
-                    <ul id="menuitems">
-                        <li><a href="index.html">Home</a></li>
-                        <li><a href="product.html">Products</a></li>
-                        <li><a href="about.html">About</a></li>
-                        <li><a href="contact.html">Contact</a></li>
-                        <li><a href="account.html">Account</a></li>
-                    </ul>
-                </nav>
-                <a href="productdetails.html" href="cart.html"><img src="assets/images/CART.png" width="20px" height="20px"></a>
-                <img src="assets/images/menu33.png" class="menu-icon" onclick="menutoggle()">
+    <div class="navigation">
+        <div class="navbar">
+            <div class="logo">
+                <a href="index.html"><img src="assets/images/logo.png" width="125px"></a>
             </div>
+            <nav>
+                <ul id="menuitems">
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="product.html">Products</a></li>
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                    <li><a href="account.html">Account</a></li>
+                </ul>
+            </nav>
+            <a href="productdetails.html" href="cart.html"><img src="assets/images/CART.png" width="20px" height="20px"></a>
+            <img src="assets/images/menu33.png" class="menu-icon" onclick="menutoggle()">
         </div>
-        <div class="small-container">
-            <div class="row row-2">
-                <h2>All Products</h2>
-                <select>
-                    <option>Deafault Sorting</option>
-                    <option>Sort by Price</option>
-                    <option>Sort by Popularity</option>
-                    <option>Sort by Rating</option>
-                    <option>Sort by Sale</option>
-                </select>
-            </div>
-            <?php
-            // var_dump($_SESSION['wishlist']);
-            //  var_dump(Request::getwishlist());
 
-            //   Her i get the products in the Database
+    </div>
+    <div class="small-container">
+        <div class="row row-2">
+            <h2>All Products</h2>
+            <select>
+                <option>Deafault Sorting</option>
+                <option>Sort by Price</option>
+                <option>Sort by Popularity</option>
+                <option>Sort by Rating</option>
+                <option>Sort by Sale</option>
+            </select>
+        </div>
+        <?php
+        //   Her i get the products in the Database
+        $products = App::get('database')->selectAll('products');
+        ?>
 
-            $products = App::get('database')->selectAll('products');
+        <section class="text-gray-600 body-font">
+            <div class="container px-5 py-8 mx-auto">
+                <div class="flex flex-wrap -m-4">
+                    <?php foreach ($products as $product) : ?>
+                        <div class="p-4 md:w-1/3">
+                            <div class="h-full  rounded-lg overflow-hidden">
+                                <img class="lg:h-48 md:h-36 mx-auto rounded-md  object-cover object-center" src="<?= $product['product_img'] ?>" alt="blog">
+                                <div class="p-6 border-r-2 ">
+                                    <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
+                                    <h1 class="title-font text-lg font-medium text-gray-900 mb-3"><?= $product['product_name'] ?></h1>
 
+                                    <p>ksh.<?= $product['product_price'] ?>.00</p>
+                                    <div class="flex items-center flex-wrap ">
+                                        <a href="productdetail.php?product=<?= $product['product_id'] ?>" class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">View Product
 
+                                        </a>
+                                        <span class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
 
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                                            </svg>
 
+                                        </span>
+                                        <span class="text-gray-400 inline-flex items-center leading-none text-sm">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                                            </svg>
 
-            ?>
-
-
-
-
-
-
-
-
-
-
-
-
-            <section class="text-gray-600 body-font">
-                <div class="container px-5 py-24 mx-auto">
-                    <div class="flex flex-wrap -m-4">
-                        <?php foreach ($products as $product) : ?>
-                            <div class="p-4 md:w-1/3">
-                                <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                                    <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="<?= $product['product_img'] ?>" alt="blog">
-                                    <div class="p-6">
-                                        <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
-                                        <h1 class="title-font text-lg font-medium text-gray-900 mb-3"><?= $product['product_name'] ?></h1>
-
-                                        <p>ksh.<?= $product['product_price'] ?>.00</p>
-                                        <div class="flex items-center flex-wrap ">
-                                            <a href="productdetail.php?product=<?= $product['product_id'] ?>" class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">View Product
-
-                                            </a>
-                                            <span class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
-                                                <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-                                                    <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                                                    <circle cx="12" cy="12" r="3"></circle>
-                                                </svg>
-                                            </span>
-                                            <span class="text-gray-400 inline-flex items-center leading-none text-sm">
-                                                <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-                                                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                                                </svg>
-                                            </span>
-                                        </div>
+                                        </span>
                                     </div>
                                 </div>
                             </div>
-                        <?php endforeach ?>
-                    </div>
+                        </div>
+                    <?php endforeach ?>
                 </div>
-            </section>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-            <div class="page-btn">
-                <span>1</span>
-                <span>2</span>
-                <span>3</span>
-                <span>4</span>
-                <span>&#8594</span>
             </div>
+        </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <div class="page-btn">
+            <span>1</span>
+            <span>2</span>
+            <span>3</span>
+            <span>4</span>
+            <span>&#8594</span>
         </div>
     </div>
+
     <!-----footer-->
     <div class="footer">
         <div class="container">
